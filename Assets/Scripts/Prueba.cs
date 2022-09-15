306b59d8b9c372f3b33898af2738827225cdc9a3
SebasLam3 <90872517+SebasLam3@users.noreply.github.com>
2022-09-14 20:31:41 -0500

    Prueba

---
+++ Assets/Scripts/Prueba.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prueba : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Script de prueba 
+        for (int i = 0; i < 3; i++)
+        {
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
